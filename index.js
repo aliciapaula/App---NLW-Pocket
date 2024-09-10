aeefe8fd2861a686ae58cfb104d3c7121a37dead
Bowie <marketing@letscode.com.br>
2024-09-10 15:05:01 -0300

    fundamentos 001

---
+++ index.js
@@ -0,0 +1,15 @@
+// // Hello World!
+// // let - pode mudar
+// //const - não pode mudar
+// const mensagem = "olá eu"
+
+// {
+//     let mensagem = "olá, mundo!"
+//     console.log(mensagem);
+// }
+
+// console.log(mensagem);
+
+// arrays, objetos
+let 
+
